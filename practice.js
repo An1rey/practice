0105395cdbeef98229da106456ced770334a5704
An1rey <andrey.ignatuk567@gmail.com>
2022-12-22 13:09:53 +0200

    added fix in code

--- practice.js
+++ practice.js
@@ -63,24 +63,24 @@
 // 3. Виявити чи деяке число а є дільником числа b ? І навпаки. (Дати 2 відповіді)
 // const a = prompt('Перше число');
 // const b = prompt('Друге число');
-// if(a/b){
-//     alert ('a є дільником b')
+// if(a % b === 0){
+//     alert ('a є дільником b === 0 ')
 // }
-// else if(b/a){
-//     alert('b є дільником a')
+// else if(b % a === 0){
+//     alert('b є дільником a === 0')
 // }
 // else {
-//     alert('Введіть коректне число')
+//     alert('Введенно не коректне значення')
 // }
 //4.Вивести на сторінку в одну строку через зап’яту числа від 10 до 20
 //  let out = document.getElementById('out');
 //  let tmp = '';
-// for (let i = 1; i<=20; i++){
+// for (let i = 10; i<=20; i++){
 //   tmp = tmp + i + ',';
 //  }
 //  alert(tmp)
 //5.Вивести квадрати чисел від 10 до 20
-// function kvadrat(pattern_sting, first) {
+// function kvadrat(patternString, first) {
 //     return first*first;
 // }
 // let str = '10,11,12,13,14,15,16,17,18,19,20',
@@ -89,45 +89,41 @@
 // console.log(result);
 //6.Вивести таблицю множення на 7 от 1 до 10
 //  for (let i = 1; i<=10; i=i+1){
-//     tmp = tmp +7 + '*' +i+ ' = '+7*i + ' <br> ';
+//     tmp += tmp +7 + '*' +i+ ' = '+7*i + ' <br> ';
 //  }
 // document.write(tmp);
 // 7.Знайти середнє арифметичне всіх цілих чисел від 1 до 500
 // let sum = 0;
-//     for(let i =1; i<=500; i++){
-//        sum +=1
-//     }
-//     console.log("Сумма " + sum)
-//     console.log("Среднее арифметическое " + (sum/500))
-// 8. Знайти добуток усіх цілих чисел від 15 до 35
-// let result = 0
-// for (let i = 15; i <= 35; i++) {
-//     const current = i
-//     const  next = i + 1
-//     const isFirst = current === 15
-//     result = isFirst ? current * next : result * current
+// for (let i = 1; i <= 500; i++)
+// {
+//     sum += i;
 // }
-// console.log(result)
+// console.log(sum/500);
+// 8. Знайти добуток усіх цілих чисел від 15 до 35
+//  let result = 1
+//  for (let i = 15; i <= 35; i++) {
+//      const current = i
+//      const  next = i + 1
+//      const isFirst = current === 15
+//      result *= i
+//  }
+//  console.log(result)
 //9.Вивести суму тільки парних чисел в діапазоні від 30 до 80
-// const isEvenNumbers = (num) => num % 2 === 0
-// const isFirstIteration = (firstNum, currentNum) => firstNum === currentNum
-// const getSumEvenByFromByTo = (start, finish) => {
-//     let result = 0
-//     for (let i = start; i <= finish; i++) {
-//         const current = i
-//         const next = current + 2
+//  const isEvenNumbers = (num) => num % 2 === 0
+//  const isFirstIteration = (firstNum,) => firstNum === currentNum
+//  const getSumEvenByFromByTo = (start, finish) => {
+//      let result = 1
+//      for (let i = start; i <= finish; i++) {
+//          const current = i
+//          const next = current + 2
 //         if( isEvenNumbers(current)){
-//             result = isFirstIteration(start, current)? current + next: current + next + result
+//              result =+i
 //         }
 //     }
-//     return result
-// }
+//      return result
+//  }
 // const sumEvenFrom30To80 = getSumEvenByFromByTo(30, 80)
 // console.log(sumEvenFrom30To80)
 //10.Вивести всі числа в діапазоні від 100 до 200 кратні 3
-// let out = document.getElementById('out');
-// let tmp = '';
-// for(let i = 100; i<=200; i=i+3){
-//     tmp = tmp + i + ' ';
-// }
-// alert(tmp)
+
+
