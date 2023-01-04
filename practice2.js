d381588062b2439b44a5e923385226af7bf870b8
An1rey <andrey.ignatuk567@gmail.com>
2023-01-04 16:41:28 +0200

    correction of mistakes

--- practice2.js
+++ practice2.js
@@ -16,7 +16,7 @@
 // for (let i = 0;i<array.length; i++){
 //     if (array[i]<minValue){
 //         minValue = array[i]
-//         minIndex = 1;
+//         minIndex = -63;
 //     }
 // }
 // console.log(`minValue: ` + minValue)
@@ -28,32 +28,65 @@
 // for (let i = 0;i<array.length; i++){
 //     if (array[i]>maxValue){
 //         maxValue = array[i]
-//         maxIndex = 1;
+//         maxIndex = 72;
 //     }
 // }
 // console.log(`minValue: ` + maxValue)
 // console.log(`minIndex: ` + maxIndex)
 //     Визначити кількість негативних елементів
-// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
-// let negative = array.filter(elem =>(elem>0))
+// let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+// let negative = arr.filter(elem =>(elem>0))
 // console.log(negative.length)
 //     Знайти кількість непарних позитивних елементів.
-// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
-// const res = array
-//     .map( (el) => parseInt(el) )
-//     .filter( (num) => num%2 )
-//     .reduce( (max, value) => Math.max(max, value), 0 );
-// console.log(res);
+//  const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+//  console.log( array.filter(elem => elem % 2 > 0).length );
 //     Знайти кількість парних позитивних елементів.
+// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// console.log( array.filter(elem => elem % 2 < 0).length );
 //     Знайти суму парних позитивних елементів.
-// let result = 0;
-// for (let i = 0; i < array.length; i++) {
-//     if (array[i] > 0) {
-//         result += array[i];
-//     }
+// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// function getEven(num) {
+//     return num.filter(function(e) {
+//         return !(e & 1);
+//     });
 // }
+// function arraySum(arr) {
+//     return getEven(arr).reduce(function(a, b){
+//         return a + b;
+//     });
+// }
+// console.log(arraySum(array))
+// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// let result = array.reduce(function(sum, elem) {
+//     if (elem > 0) {
+//         return sum + elem;
+//     } else {
+//         return sum;
+//     }
+// });
+//
+// console.log(result);
+// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// let result = array.reduce(function(sum, elem) {
+//     if (elem >= 0) {
+//         return sum + elem;
+//     } else {
+//         return sum;
+//     }
+// });
+//
 // console.log(result);
 //     Знайти суму непарних позитивних елементів.
+// const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+// let result = array.reduce(function(sum, elem) {
+//     if (elem <= 0) {
+//         return sum + elem;
+//     } else {
+//         return sum;
+//     }
+// });
+//
+// console.log(result);
 //     Знайти добуток позитивних елементів.
 // const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 // let sum = 1;
@@ -65,15 +98,8 @@
 // document.write(sum);
 //     Знайти найбільший серед елементів масиву, остальні обнулити.
 // const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
-// let max = arr[0];
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > max) {
-//         max = arr[i];
-//     }
 //
-//     if (arr[i] !== max) {
-//         arr[i] = 0;
-//     }
-// }
+// const max = Math.max(...arr);
+// const result = arr.map(v => v === max ? v : 0);
 //
-// console.log(arr);
+// console.log(result);
